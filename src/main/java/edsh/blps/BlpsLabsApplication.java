433102a3c7e9fe93d544a5fdacf95a750104ab3a
edshPC <ed_shh@mail.ru>
2025-02-21 15:17:46 +0300

    Init

---
+++ src/main/java/edsh/blps/BlpsLabsApplication.java
@@ -0,0 +1,13 @@
+package edsh.blps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlpsLabsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlpsLabsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edsh/blps/BlpsLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package edsh.blps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlpsLabsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
