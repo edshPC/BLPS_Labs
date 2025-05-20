bb12be5b68098e9036c5b83774560edfe7cfc7f0
edshPC <ed_shh@mail.ru>
2025-05-20 15:15:20 +0300

    PickpointTask

---
+++ Order/src/main/java/edsh/blps/camunda/GetPickpointsTask.java
@@ -0,0 +1,24 @@
+package edsh.blps.camunda;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edsh.blps.config.BeanProvider;
+import edsh.blps.controller.MessageSender;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class GetPickpointsTask implements JavaDelegate {
+    private final ObjectMapper objectMapper;
+    private final MessageSender messageSender;
+
+    public GetPickpointsTask() {
+        messageSender = BeanProvider.getBean(MessageSender.class);
+        objectMapper = BeanProvider.getBean(ObjectMapper.class);
+    }
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        var result = messageSender.getAllPickPoint();
+        var array = result.stream().map(pick -> pick.getAddress().getAddress()).toList();
+        delegateExecution.setVariable("pickpoints", objectMapper.writeValueAsString(array));
+    }
+}
