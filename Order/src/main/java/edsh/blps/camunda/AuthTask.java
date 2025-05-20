42e817647eae9710923851f7920ea6945289e69d
VictorEydelman <113546427+VictorEydelman@users.noreply.github.com>
2025-05-20 14:06:45 +0300

    AuthTask

---
+++ Order/src/main/java/edsh/blps/camunda/AuthTask.java
@@ -0,0 +1,34 @@
+package edsh.blps.camunda;
+
+import edsh.blps.config.BeanProvider;
+import edsh.blps.dto.OrderDTO;
+import edsh.blps.dto.UserDTO;
+import edsh.blps.entity.primary.User;
+import edsh.blps.service.OrderService;
+import edsh.blps.service.UserService;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class AuthTask implements JavaDelegate {
+
+    private final UserService userService;
+
+    public AuthTask() {
+        userService = BeanProvider.getBean(UserService.class);
+    }
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        var username = (String) delegateExecution.getVariable("username");
+        var password = (String) delegateExecution.getVariable("password");
+        UserDTO userDTO = new UserDTO(username,password,null);
+        try {
+            var result = userService.login(userDTO);
+            delegateExecution.setVariable("auth", true);
+            delegateExecution.setVariable("link", result);
+        } catch (Exception e){
+            delegateExecution.setVariable("auth", false);
+            delegateExecution.setVariable("link", null);
+        }
+    }
+}
